e742761f9e90e4f39c86a8d7e2be8b7683e2107d
Jay-Plumb <jaymjplumb@gmail.com>
2016-02-12 12:59:57 +0000

    finished up segue controller

---
+++ pokedex/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Jason Plumb on 2016. 2. 12..
+//  Copyright © 2016년 JasonPlumb. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v2/pokemon/"
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -12,6 +12,16 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLbl.text = pokemon.name
@@ -19,5 +29,8 @@ class PokemonDetailVC: UIViewController {
 
 
 
+    @IBAction func backbtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }

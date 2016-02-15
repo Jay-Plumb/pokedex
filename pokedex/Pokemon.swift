8145a91ffd342d28e8c8f78d5ea21c58bc867cfd
Jay-Plumb <jaymjplumb@gmail.com>
2016-02-15 19:16:42 +0000

    populated data to detailView and tied up

--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -19,6 +19,8 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionText: String!
+    private var _nextEvolutionId: String!
+    private var _nextEvolutionLvl: String!
     private var _pokemonUrl: String!
     
     
@@ -30,6 +32,65 @@ class Pokemon {
         return _pokedexId
     }
     
+    var description: String {
+        if _description == nil {
+            _description = ""
+        }
+        return _description
+    }
+    
+    var type: String {
+        return _type
+    }
+    
+    var defense: String {
+        if _defense == nil {
+            _defense = ""
+        }
+        return _defense
+    }
+    
+    var height: String {
+        if _height == nil {
+            _height = ""
+        }
+        return _height
+    }
+    
+    var weight: String {
+        if _weight == nil {
+            _weight = ""
+        }
+        return _weight
+    }
+    
+    var attack: String {
+        if _attack == nil {
+           _attack = ""
+        }
+        return _attack
+    }
+    
+    var nextEvolutionText: String {
+        if _nextEvolutionText == nil {
+            _nextEvolutionText = ""
+        }
+        return _nextEvolutionText
+    }
+    
+    var nextEvolutionId: String {
+        if _nextEvolutionId == nil {
+            _nextEvolutionId = ""
+        }
+        return _nextEvolutionId
+    }
+    
+    var nextEvolutionLvl: String {
+        if _nextEvolutionLvl == nil {
+            _nextEvolutionLvl = ""
+        }
+        return _nextEvolutionLvl
+    }
     init(name: String, pokedexId: Int) {
         _name = name
         _pokedexId = pokedexId
@@ -78,6 +139,52 @@ class Pokemon {
                 }
                 print(self._type)
                 
+                // Description
+                if let descArr = dict["descriptions"] as? [Dictionary <String, String>] where descArr.count > 0{
+                    if let url = descArr[0]["resource_uri"] {
+                        let nsurl = NSURL(string: "\(URL_BASE)\(url)")!
+                        // Make another request
+                        Alamofire.request(.GET, nsurl).responseJSON{ (response) -> Void in
+                            let result = response.result
+                            if let descDict = result.value as? Dictionary<String, AnyObject> {
+                                if let description = descDict["description"] as? String {
+                                    self._description = description
+                                    print(self._description)
+                                }
+                            }
+                            completed()
+                        }
+                    }
+                } else {
+                    self._description = ""
+                }
+                
+                // Evolutions
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] where evolutions.count > 0{
+                    if let to = evolutions[0]["to"] as? String {
+                        // Remove mega pokemons as we dont support these
+                        // mega not found so good
+                        if to.rangeOfString("mega") == nil {
+                            // grab id
+                            if let uri = evolutions[0]["resource_uri"] as? String {
+                                let newStr = uri.stringByReplacingOccurrencesOfString("/api/v1/pokemon/", withString: "")
+                                let num = newStr.stringByReplacingOccurrencesOfString("/", withString: "")
+                                
+                                self._nextEvolutionId = num
+                                self._nextEvolutionText = to
+                                
+                                if let lvl = evolutions[0]["level"] as? Int {
+                                    self._nextEvolutionLvl = "\(lvl)"
+                                }
+                                
+                                print(self._nextEvolutionId)
+                                print(self._nextEvolutionText)
+                                print(self._nextEvolutionLvl)
+                            }
+                            
+                        }
+                    }
+                }
             }
         }
             
--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -19,16 +19,49 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var heightLbl: UILabel!
     @IBOutlet weak var pokedexLbl: UILabel!
     @IBOutlet weak var nextEvoImg: UIImageView!
-    
     @IBOutlet weak var currentEvoImg: UIImageView!
     @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttack: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLbl.text = pokemon.name
-        mainImg.image = UIImage(named: "\(pokemon.pokedexID)")
+        let img = UIImage(named: "\(pokemon.pokedexID)")
+        mainImg.image = img
+        currentEvoImg.image = img
+
         pokemon.downloadPokemonDetails { () -> () in
             // This will be called after download is done
+            self.updateUI()
+            
         }
+    }
+    
+    func updateUI() {
+        
+        descriptionLbl.text = pokemon.description
+        typeLbl.text = pokemon.description
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        pokedexLbl.text = "\(pokemon.pokedexID)"
+        weightLbl.text = pokemon.weight
+        baseAttack.text = pokemon.attack
+        
+        if pokemon.nextEvolutionId == "" {
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.hidden = true
+        } else {
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var str = "Next Evolution: \(pokemon.nextEvolutionText)"
+            if pokemon.nextEvolutionLvl != "" {
+                str += " - LEVEL: \(pokemon.nextEvolutionLvl)"
+                evoLbl.text = str
+            }
+        }
+        
+      
     }
 
 
